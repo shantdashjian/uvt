b1de2ed3c9066644591658d3f73c81627ba22066
Shaun Dashjian <shaundashjian@yahoo.com>
2019-06-07 23:44:00 -0700

    Added Bootstrap dependencies and fragments list

--- WebContent/app/fragment/fragmentList/fragmentList.component.js
+++ WebContent/app/fragment/fragmentList/fragmentList.component.js
@@ -1,4 +1,23 @@
 angular.module('fragment')
 	.component('fragmentList', {
-		templateUrl: 'app/fragment/fragmentList/fragmentList.component.html'
+		templateUrl : 'app/fragment/fragmentList/fragmentList.component.html',
+		controller : function() {
+			var vm = this;
+			
+			vm.fragments = [
+				{
+					startTime : 0,
+					endTime : 10000
+				},
+				{
+					startTime : 10000,
+					endTime : 20000
+				},
+				{
+					startTime : 20000,
+					endTime : 30000
+				}
+			];
+		},
+		controllerAs : 'vm'
 	});
